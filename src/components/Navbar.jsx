ab5edcf8bc54902e3a737999f78526962de6dad1
znobakht <nobakht9010@yahoo.com>
2024-04-29 18:36:55 +0330

    fixing navbar on the top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="bg-blue-500 p-6">
+    <nav className="bg-blue-500 p-6 fixed top-0 left-0 right-0 z-10">
       <div className="container mx-auto flex justify-between items-center">
         {/* <Link to="/" className="text-white text-xl font-bold">
           Home
